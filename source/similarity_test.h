d67fee5c79748d593d638dc00cdd2f12cad5728a
Marek Topolewski <marek.topolewski@gmail.com>
2019-02-24 11:15:59 +0000

    clean up similairty test header file

--- source/similarity_test.h
+++ source/similarity_test.h
@@ -1,13 +1,9 @@
-//
-// Created by marek on 24/02/2019.
-//
-
-#ifndef MCTSF_ECPLISE_SIMILARITY_TEST_H
-#define MCTSF_ECPLISE_SIMILARITY_TEST_H
+#ifndef SIMILARITY_TEST_H_
+#define SIMILARITY_TEST_H_
 
 /**
  * Method called from an UCI command to begin similairty measure testing
  */
 void similarityTest();
 
-#endif //MCTSF_ECPLISE_SIMILARITY_TEST_H
+#endif /* SIMILARITY_TEST_H_ */
